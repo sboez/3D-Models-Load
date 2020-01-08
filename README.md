33a4107786c240531c5db3c9810cd0c1015fdfb2
Sandra <sandra.boez@gmail.com>
2020-01-08 13:41:52 +0100

    Add FBX/GLTF Loaders and car/motorbike models

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 3D-Models-Load
-Load your 3D model and play with it thanks to Three.js and Cannon.js 
+Load your 3D model FBX or GLTF/GLB easily in a Three.js scene
+
+I'm using Blender to export my FBX file to a GLB. 
+To add a texture on your FBX model with Blender, you need to check *copy* in *Path Mode* during export.
--- scripts/main.js
+++ scripts/main.js
@@ -19,19 +19,21 @@
 		document.body.appendChild(container);
 
 		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-		camera.position.set(300, 750, 900);
+		camera.position.set(250, 180, 280);
 
 		scene = new THREE.Scene();
 		scene.background = new THREE.Color(0xa0a0a0);
 
-		let light = new THREE.AmbientLight(0x404040, 20); 
-		light.position.set(0, 10, 0);
+		let light = new THREE.HemisphereLight(0xffffff, 0x404040, 1); 
 		scene.add(light);
 
-		let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(1000, 1000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+		let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(400, 400), new THREE.MeshPhongMaterial({ color: 0xcfcfcf, depthWrite: false }));
 		mesh.rotation.x = -Math.PI / 2;
 		mesh.receiveShadow = true;
 		scene.add(mesh);
+
+		/*** Change the model here to add your ***/
+		loadGltf('assets/models/gltf/street_car.glb');
 
 		renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setPixelRatio(window.devicePixelRatio);
@@ -43,6 +45,37 @@
 		controls.update();
 
 		window.addEventListener('resize', onWindowResize, false);
+	}
+
+	function loadFbx(path) {
+		var loader = new THREE.FBXLoader();
+		loader.load(path, function(object) {
+			object.traverse( function (child) {
+				if (child.isMesh) {
+					child.castShadow = true;
+					child.receiveShadow = true;
+				}
+			});
+			scene.add(object);
+		});
+	}
+
+	function loadGltf(path) {
+		return new Promise((resolve, reject) => {
+			let loader = new THREE.GLTFLoader();
+			loader.load(path, function(gltf) {
+				gltf.scene.traverse(function(child) {
+					if (child.isMesh) {
+						child.castShadow = true;
+						child.receiveShadow = true;
+					}
+				});
+				/*** Rescale your model if needed ***/
+				gltf.scene.scale.set(100,100,100);
+				scene.add(gltf.scene);
+				return resolve(gltf.scene);
+			});
+		})
 	}
 
 	function onWindowResize() {
